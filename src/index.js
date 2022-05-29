34a17a9ca4a63f44343d928cacbe13f0e26f8962
Nikita Tinaev <Nikkach1997@yandex.ru>
2022-05-29 23:29:52 +0300

    feat: создай инфрастурктуру, установи CRA, удали лишнее

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './vendor/fonts/fonts.css'
+import './vendor/normalize.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
