c254e81dedf51eeed9eac3d88b0175236ebb3fa8
Nikita Tinaev <Nikkach1997@yandex.ru>
2022-06-06 12:13:21 +0300

    feat: сверстай компонент Techs (без адаптива)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,15 @@
 import './Main.css';
 import Promo from '../Promo/Promo.jsx'
 import AboutProject from '../AboutProject/AboutProject.jsx';
+import Techs from '../Techs/Techs.jsx';
 
 export default function Main() {
   return (
     <main className="main">
       <Promo />
       <AboutProject />
-      {/* <Techs />
-      <AboutMe />
+      <Techs />
+      {/* <AboutMe />
       <Portfolio /> */}
     </main>
   );
