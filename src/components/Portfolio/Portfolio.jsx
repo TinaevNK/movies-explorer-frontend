46940fd201ff379971e4a383da00f9f47e5514a5
Nikita Tinaev <Nikkach1997@yandex.ru>
2022-06-07 19:30:21 +0300

    feat: сверстай компонент Portfolio (без адаптива)

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,43 @@
+import "./Portfolio.css";
+
+export default function Portfolio() {
+  return (
+    <section className="portfolio">
+      <div className="portfolio__container">
+        <h2 className="portfolio__title">Портфолио</h2>
+        <ul className="portfolio__projects-list">
+          <li className="portfolio__projects-item">
+            <a
+              href="https://github.com/TinaevNK/how-to-learn"
+              target="_blank"
+              rel="noreferrer"
+              className="portfolio__link"
+            >
+              Статичный сайт
+            </a>
+          </li>
+          <li className="portfolio__projects-item">
+            <a
+              href="https://github.com/TinaevNK/russian-travel"
+              target="_blank"
+              rel="noreferrer"
+              className="portfolio__link"
+            >
+              Адаптивный сайт
+            </a>
+          </li>
+          <li className="portfolio__projects-item">
+            <a
+              href="https://github.com/TinaevNK/react-mesto-api-full"
+              target="_blank"
+              rel="noreferrer"
+              className="portfolio__link"
+            >
+              Одностраничное приложение
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
