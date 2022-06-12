1a8eb7c1dc1616b9f59f43f267bb0eb1986ee8c4
Nikita Tinaev <Nikkach1997@yandex.ru>
2022-06-13 02:52:03 +0300

    fix: пофикси проблемы Register и Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -18,55 +18,53 @@ export default function Login() {
 
   return (
     <main className="login">
-      <div className="login__container">
-        <form className="login__form" noValidate onSubmit={handleSubmit}>
-          <Link to="/" className="login__link">
-            <img src={logo} alt="Логотип" className="login__logo" />
-          </Link>
-          <h1 className="login__title">Рады видеть!</h1>
-          <div className="login__labels-container">
-            <label className="login__label">
-              <span className="login__label-text">E-mail</span>
-              <input
-                name="email"
-                className={`login__input ${errors.email && 'login__input_error'}`}
-                onChange={handleChange}
-                value={values.email || ''}
-                type="email"
-                required
-              />
-              <span className="login__error">{errors.email || ''}</span>
-            </label>
-            <label className="login__label">
-              <span className="login__label-text">Пароль</span>
-              <input
-                name="password"
-                className={`login__input ${errors.password && 'login__input_error'}`}
-                onChange={handleChange}
-                value={values.password || ''}
-                type="password"
-                required
-              />
-              <span className="login__error">{errors.password || ''}</span>
-            </label>
-          </div>
-          <button
-            type="submit"
-            className={`login__button ${
-              !isValid && 'login__button_disabled'
-            }`}
-            disabled={!isValid}
-          >
-            Войти
-          </button>
-        </form>
+      <form className="login__form" noValidate onSubmit={handleSubmit}>
+        <Link to="/" className="login__link">
+          <img src={logo} alt="Логотип" className="login__logo" />
+        </Link>
+        <h1 className="login__title">Рады видеть!</h1>
+        <div className="login__labels-container">
+          <label className="login__label">
+            <span className="login__label-text">E-mail</span>
+            <input
+              name="email"
+              className={`login__input ${errors.email && 'login__input_error'}`}
+              onChange={handleChange}
+              value={values.email || ''}
+              type="email"
+              required
+            />
+            <span className="login__error">{errors.email || ''}</span>
+          </label>
+          <label className="login__label">
+            <span className="login__label-text">Пароль</span>
+            <input
+              name="password"
+              className={`login__input ${errors.password && 'login__input_error'}`}
+              onChange={handleChange}
+              value={values.password || ''}
+              type="password"
+              required
+            />
+            <span className="login__error">{errors.password || ''}</span>
+          </label>
+        </div>
+        <button
+          type="submit"
+          className={`login__button ${
+            !isValid && 'login__button_disabled'
+          }`}
+          disabled={!isValid}
+        >
+          Войти
+        </button>
         <span className="login__support">
           Ещё не зарегистрированы?&nbsp;
           <Link to="signup" className="login__link">
             Регистрация
           </Link>
         </span>
-      </div>
+      </form>
     </main>
   )
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -18,69 +18,67 @@ export default function Register() {
 
   return (
     <main className="register">
-      <div className="register__container">
-        <form className="register__form" noValidate onSubmit={handleSubmit}>
-          <Link to="/" className="register__link">
-            <img src={logo} alt="Логотип" className="register__logo" />
-          </Link>
-          <h1 className="register__title">Добро пожаловать!</h1>
-          <div className="register__labels-container">
-            <label className="register__label">
-              <span className="register__label-text">Имя</span>
-              <input
-                name="name"
-                className={`register__input ${errors.name && 'register__input_error'}`}
-                onChange={handleChange}
-                value={values.name || ''}
-                type="text"
-                required
-                minLength="2"
-                maxLength="30"
-              />
-              <span className="register__error">{errors.name || ''}</span>
-            </label>
-            <label className="register__label">
-              <span className="register__label-text">E-mail</span>
-              <input
-                name="email"
-                className={`register__input ${errors.email && 'register__input_error'}`}
-                onChange={handleChange}
-                value={values.email || ''}
-                type="email"
-                required
-              />
-              <span className="register__error">{errors.email || ''}</span>
-            </label>
-            <label className="register__label">
-              <span className="register__label-text">Пароль</span>
-              <input
-                name="password"
-                className={`register__input ${errors.password && 'register__input_error'}`}
-                onChange={handleChange}
-                value={values.password || ''}
-                type="password"
-                required
-              />
-              <span className="register__error">{errors.password || ''}</span>
-            </label>
-          </div>
-          <button
-            type="submit"
-            className={`register__button ${
-              !isValid && 'register__button_disabled'
-            }`}
-            disabled={!isValid}
-          >
-            Зарегистрироваться
-          </button>
-        </form>
+      <form className="register__form" noValidate onSubmit={handleSubmit}>
+        <Link to="/" className="register__link">
+          <img src={logo} alt="Логотип" className="register__logo" />
+        </Link>
+        <h1 className="register__title">Добро пожаловать!</h1>
+        <div className="register__labels-container">
+          <label className="register__label">
+            <span className="register__label-text">Имя</span>
+            <input
+              name="name"
+              className={`register__input ${errors.name && 'register__input_error'}`}
+              onChange={handleChange}
+              value={values.name || ''}
+              type="text"
+              required
+              minLength="2"
+              maxLength="30"
+            />
+            <span className="register__error">{errors.name || ''}</span>
+          </label>
+          <label className="register__label">
+            <span className="register__label-text">E-mail</span>
+            <input
+              name="email"
+              className={`register__input ${errors.email && 'register__input_error'}`}
+              onChange={handleChange}
+              value={values.email || ''}
+              type="email"
+              required
+            />
+            <span className="register__error">{errors.email || ''}</span>
+          </label>
+          <label className="register__label">
+            <span className="register__label-text">Пароль</span>
+            <input
+              name="password"
+              className={`register__input ${errors.password && 'register__input_error'}`}
+              onChange={handleChange}
+              value={values.password || ''}
+              type="password"
+              required
+            />
+            <span className="register__error">{errors.password || ''}</span>
+          </label>
+        </div>
+        <button
+          type="submit"
+          className={`register__button ${
+            !isValid && 'register__button_disabled'
+          }`}
+          disabled={!isValid}
+        >
+          Зарегистрироваться
+        </button>
         <span className="register__support">
           Уже зарегистрированы?&nbsp;
           <Link to="signin" className="register__link">
             Войти
           </Link>
         </span>
-      </div>
+      </form>
     </main>
   )
 }
