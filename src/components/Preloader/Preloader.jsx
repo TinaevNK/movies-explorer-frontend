5e243886265a18ffbb67e8e6f4d325b84e1e4902
Nikita Tinaev <Nikkach1997@yandex.ru>
2022-06-14 03:05:55 +0300

    feat: заполни ReadMe

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,11 @@
+import './Preloader.css';
+
+export default function Preloader() {
+  return (
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  )
+}
