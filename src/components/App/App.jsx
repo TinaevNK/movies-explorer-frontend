50e54bcb547eb3ade045746ac6cb2e571fc1b45e
Nikita Tinaev <Nikkach1997@yandex.ru>
2022-06-17 22:31:29 +0300

    feat & refactor: добавь компонент высшего порядка ProtectedRoute и отрефактори код

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
-import moviesData from '../../utils/movies';
+import moviesData from '../../utils/movies.js';
 import { useState, useEffect } from 'react';
 import { Route, Switch, useHistory } from 'react-router-dom';
-import Header from '../Header/Header';
+import Header from '../Header/Header.jsx';
 import Main from '../Main/Main.jsx';
 import Footer from '../Footer/Footer.jsx';
 import Movies from '../Movies/Movies.jsx';
@@ -13,14 +13,19 @@ import Profile from '../Profile/Profile.jsx';
 import NotFound from '../NotFound/NotFound.jsx';
 import Preloader from '../Preloader/Preloader.jsx';
 import InfoTooltip from '../InfoTooltip/InfoTooltip.jsx';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute.jsx';
 
 export default function App() {
   const history = useHistory();
   const [isLoader, setIsLoader] = useState(false);
   const [isInfoTooltip, setIsInfoTooltip] = useState({ isOpen: false, successful: true, text: ''});
   const [isBurgerOpened, setIsBurgerOpened] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
   const [movies, setMovies] = useState([]);
   const [savedMovies, setSavedMovies] = useState([]);
+
+  const headerEndpoints = ['/movies', '/saved-movies', '/profile', '/'];
+  const footerEndpoints = ['/movies', '/saved-movies', '/'];
 
   // нажатие по бургерному меню
   function onClickBurger(isBurgerOpened) {
@@ -48,21 +53,12 @@ export default function App() {
 
   return (
     <div className="app">
+      <Route exact path={headerEndpoints}>
+        <Header loggedIn={loggedIn} onClickBurger={onClickBurger} isBurgerOpened={isBurgerOpened} />
+      </Route>
       <Switch>
-        <Route path="/" exact>
-          <Header themeDark={false} authorized={false} onClickBurger={onClickBurger} isBurgerOpened={isBurgerOpened} />
+        <Route exact path="/">
           <Main />
-          <Footer />
-        </Route>
-        <Route path="/movies">
-          <Header themeDark={true} authorized={true} onClickBurger={onClickBurger} isBurgerOpened={isBurgerOpened} />
-          <Movies movies={movies} />
-          <Footer />
-        </Route>
-        <Route exact path="/saved-movies">
-          <Header themeDark={true} authorized={true} onClickBurger={onClickBurger} isBurgerOpened={isBurgerOpened} />
-          <SavedMovies movies={savedMovies}/>
-          <Footer />
         </Route>
         <Route exact path="/signup">
           <Register />
@@ -70,14 +66,21 @@ export default function App() {
         <Route exact path="/signin">
           <Login />
         </Route>
-        <Route exact path="/profile">
-          <Header themeDark={true} authorized={true} onClickBurger={onClickBurger} isBurgerOpened={isBurgerOpened} />
-          <Profile />
-        </Route>
-        <Route path="*">
-          <NotFound goBack={goBack} />
-        </Route>
+        <ProtectedRoute path="/movies"
+          component={Movies}
+          movies={movies} />
+        <ProtectedRoute path=""
+          component={SavedMovies}
+          movies={savedMovies} />
+        <ProtectedRoute path="/profile"
+          component={Profile} />
+        <ProtectedRoute path="*"
+          component={NotFound}
+          goBack={goBack} />
       </Switch>
+      <Route exact path={footerEndpoints}>
+        <Footer />
+      </Route>
       <Preloader isOpen={isLoader} />
       <InfoTooltip status={isInfoTooltip} onClose={closeInfoTooltip} />
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css';
 import Navigation from '../Navigation/Navigation.jsx';
 import logo from '../../images/logo.svg';
 
-function Header({ themeDark, authorized, onClickBurger, isBurgerOpened }) {
+function Header({ authorized, onClickBurger, isBurgerOpened }) {
+  const location = useLocation();
+
   return (
-    <header className={`header header_theme_${themeDark ? 'dark' : 'bright'}`}>
+    <header className={`header header_theme_${location.pathname === "/" ? 'bright' : 'dark'}`}>
       <div className="header__container">
         <Link to="/" className="header__link">
           <img src={logo} alt="Логотип" />
